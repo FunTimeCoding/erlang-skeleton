7504c14baab379a127cc2a5d5061b1ac76ef5f2c
Alexander Reitzel <funtimecoding@gmail.com>
2015-01-04 15:28:47 +0100

    ignore beam files, added tutorial example functions

--- tut.erl
+++ tut.erl
@@ -1,5 +1,21 @@
 -module(tut).
 -export([double/1]).
+-export([fac/1, mult/2]).
+-export([convert/2]).
 
 double(X) ->
     2 * X.
+
+fac(1) ->
+    1;
+fac(N) ->
+    N * fac(N - 1).
+
+mult(X, Y) ->
+    X * Y.
+
+convert(M, inch) ->
+    M / 2.54;
+
+convert(N, centimeter) ->
+    N * 2.54.

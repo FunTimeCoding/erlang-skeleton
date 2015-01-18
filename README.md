42a1e56aa3d5559c6ac4d6628f21cf702bcd5f8e
Alexander Reitzel <funtimecoding@gmail.com>
2015-01-18 17:30:26 +0100

    adding more tutorial code, few more readme commands and a src/test directory

--- README.md
+++ README.md
@@ -5,3 +5,15 @@ Start erlang shell.
 ```sh
 erl
 ```
+
+Compile a module.
+
+```sh
+erl -compile hello.erl
+```
+
+Run a function that has no arguments.
+
+```sh
+erl -s hello hello_world
+```
--- src/hello.erl
+++ src/hello.erl
@@ -1,7 +1,8 @@
--module(tut).
+-module(hello).
 -export([double/1]).
 -export([fac/1, mult/2]).
 -export([convert/2]).
+-export([hello_world/0]).
 
 double(X) ->
     2 * X.
@@ -19,3 +20,5 @@ convert(M, inch) ->
 
 convert(N, centimeter) ->
     N * 2.54.
+
+hello_world() -> io:fwrite("hello, world\n").
